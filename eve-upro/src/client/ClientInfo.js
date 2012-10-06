55745021736a85288ea08a4bce27186598f633d4
Christian Haas <christian.haas@sevensuns.at>
2012-10-06 14:50:30 +0200

    #149 consequential use of the MIT license; refined disclaimer at login

--- eve-upro/src/client/ClientInfo.js
+++ eve-upro/src/client/ClientInfo.js
@@ -1,19 +1,21 @@
 var path = require('path');
 
-exports.header = "/*\n" + " * Copyright (c) 2011-2012 Christian Haas\n" + " *\n"
-      + " * This software is provided 'as-is', without any express or implied\n"
-      + " * warranty. In no event will the authors be held liable for any damages\n"
-      + " * arising from the use of this software.\n" + " *\n"
-      + " * Permission is granted to anyone to use this software for any purpose,\n"
-      + " * including commercial applications, and to alter it and redistribute it\n"
-      + " * freely, subject to the following restrictions:\n" + " *\n"
-      + " *    1. The origin of this software must not be misrepresented; you must not\n"
-      + " *    claim that you wrote the original software. If you use this software\n"
-      + " *    in a product, an acknowledgment in the product documentation would be\n"
-      + " *    appreciated but is not required.\n" + " *\n"
-      + " *    2. Altered source versions must be plainly marked as such, and must not be\n"
-      + " *    misrepresented as being the original software.\n" + " *\n"
-      + " *    3. This notice may not be removed or altered from any source\n" + " *    distribution.\n" + " */\n";
+exports.header = "/*\n"
+      + " * Copyright (C) 2011-2012 Christian Haas\n"
+      + " *\n"
+      + " * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated\n"
+      + " * documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the\n"
+      + " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to\n"
+      + " * permit persons to whom the Software is furnished to do so, subject to the following conditions:\n"
+      + " *\n"
+      + " * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the\n"
+      + " * Software.\n"
+      + " *\n"
+      + " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE\n"
+      + " * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n"
+      + " * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR\n"
+      + " * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
+      + " */\n";
 
 exports.sourceFiles = [];
 
